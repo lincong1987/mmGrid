bbec73a4c23a82d6579e7f187f1353c167b1e0b2
qincheng <qincheng@unitedstack.com>
2014-01-22 17:12:02 +0800

    update!

--- mmGrid.js
+++ mmGrid.js
@@ -14,15 +14,41 @@ define(["avalon", "text!mmGrid.html"], function(avalon, html) {
         var max = Math.min(rawDatas.length, options.maxRows)
         var total = rawDatas.length
         avalon.log("只显示" + max + "行")
+        function makeColumns(array) {
+            var ret = []
+            for (var i = 0, el; el = array[i++]; ) {
+                if (typeof el == "string") {
+                    el = {
+                        field: el
+                    }
+                }
+                el.text = el.text || el.field
+                el.title = options.getColumnTitle(el)
+                el.width = el.width || options.columnWidth
+                el.className = el.className || ""
+                el.sortable = !!el.sortable
+                el.toggle = true
+                ret.push(el)
+            }
+            return ret
+        }
         var top = 0
         var model = avalon.define(data.gridId, function(vm) {
             vm.active = options.active;
             vm.rows = []
+            vm.columns = makeColumns(options.columns)
             vm.viewportHeight = max * options.rowHeight
             vm.realHeight = total * options.rowHeight
             vm.srollTop = 0
             vm.min = 0
             vm.total = total
+            vm.getCellWidth = function(name){
+                for(var i =0, el; el = vm.columns[i++];){
+                    if(el.field === name){
+                        return el.width
+                    }
+                }
+            }
             vm.sss = function(e) {
                 top = this.scrollTop
                 var min = Math.floor(top / options.rowHeight)
@@ -47,7 +73,11 @@ define(["avalon", "text!mmGrid.html"], function(avalon, html) {
     widget.defaults = {
         maxRows: 20,
         rowHeight: 25,
-        defaultColumnWidth: 80
+        headerHeight: 25,
+        columnWidth: 160,
+        getColumnTitle: function() {
+            return ""
+        }
     }
     return avalon
 })
