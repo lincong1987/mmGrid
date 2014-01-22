89bec79c1ef5168b506675408e7ad0874b01b738
qincheng <qincheng@unitedstack.com>
2014-01-22 11:51:52 +0800

    update avalon

---
+++ avalon.require.text.js
@@ -0,0 +1,37 @@
+
+if (window.require && require.config) {
+    require.config.plugins.text = function(url, y, checkDeps) {
+        var modules = avalon.modules
+        var xhr = new (self.XMLHttpRequest || ActiveXObject)("Microsoft.XMLHTTP")
+        var id = url.replace(/[?#].*/, "")
+        modules[id] = {}
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4) {
+                var status = xhr.status;
+
+                if (status > 399 && status < 600) {
+                    //An http 4xx or 5xx error. Signal an error.
+                    avalon.error(url + ' HTTP status: ' + status)
+                } else {
+                    modules[id].state = 2
+                    modules[id].exports = xhr.responseText
+                    avalon.require.checkDeps()
+                }
+            }
+        }
+        xhr.open("GET", url, true)
+        if ("withCredentials" in xhr) {
+            // Check if the XMLHttpRequest object has a "withCredentials" property.
+            // "withCredentials" only exists on XMLHTTPRequest2 objects.
+            xhr.withCredentials = true
+        }
+        xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest")
+        xhr.send()
+        return id
+    }
+}
+if (Object.keys(avalon.modules).join("").indexOf("avalon.require.text") > 0) {
+    define(["avalon"], function(avalon) {
+        return avalon
+    })
+}
---
+++ mmGrid.js
@@ -0,0 +1,48 @@
+define(["avalon", "text!mmGrid.html"], function(avalon, html) {
+
+    var useTransition = window.TransitionEvent || window.WebKitTransitionEvent
+
+    var styleEl = document.getElementById("avalonStyle")
+
+    var widget = avalon.ui.grid = function(element, data, vmodels) {
+        var $element = avalon(element), options = data.gridOptions, tabs = [],
+                tabpanels = [],
+                model, el
+        var fragment = document.createDocumentFragment()
+
+        $element.addClass(" ui-accordion ui-widget ui-helper-reset")
+        var bodyHeight = options.bodyHeight
+        var max = Math.ceil(bodyHeight / options.rowHeight)
+        // console.log(max)
+        var model = avalon.define(data.gridId, function(vm) {
+            vm.active = options.active;
+            vm.rows = []
+            vm.srollTop = 0
+            vm.sss = function(e) {
+                var top = this.scrollTop
+                var min = Math.floor(top / options.rowHeight)
+
+                var datas = options.rows.slice(min, min + max)
+                if (datas.length === max) {
+                    for (var i = 0, n = datas.length; i < n; i++) {
+                        vm.rows.set(i, datas[i])
+                    }
+                    vm.srollTop = top
+                }
+
+            }
+        })
+        var datas = options.rows.slice(0, max)
+        model.rows = datas
+        avalon.nextTick(function() {
+            element.innerHTML = html
+            avalon.scan(element, [model].concat(vmodels))
+        })
+    }
+    widget.defaults = {
+        rowHeight: 25,
+        defaultColumnWidth: 80,
+        bodyHeight: 300
+    }
+    return avalon
+})
