126d7dca9dede5745b40cd70d73c3141e8aca72d
qincheng <qincheng@unitedstack.com>
2014-01-22 13:55:28 +0800

    update mmGrid

--- mmGrid.js
+++ mmGrid.js
@@ -6,34 +6,34 @@ define(["avalon", "text!mmGrid.html"], function(avalon, html) {
 
     var widget = avalon.ui.grid = function(element, data, vmodels) {
         var $element = avalon(element), options = data.gridOptions, tabs = [],
-                tabpanels = [],
                 model, el
         var fragment = document.createDocumentFragment()
 
         $element.addClass(" ui-accordion ui-widget ui-helper-reset")
-        var bodyHeight = options.bodyHeight
         var rawDatas = options.rows
         var max = Math.min(rawDatas.length, options.maxRows)
+        var total = rawDatas.length
         avalon.log("只显示" + max + "行")
+        var top = 0
         var model = avalon.define(data.gridId, function(vm) {
             vm.active = options.active;
             vm.rows = []
-            vm.realHeight = rawDatas.length * options.rowHeight
             vm.viewportHeight = max * options.rowHeight
+            vm.realHeight = total * options.rowHeight
             vm.srollTop = 0
             vm.min = 0
+            vm.total = total
             vm.sss = function(e) {
-                var top = this.scrollTop
+                top = this.scrollTop
                 var min = Math.floor(top / options.rowHeight)
-                
-                var datas = options.rows.slice(min, min + max + 5)
-                if (datas.length >= max) {
+                if (min + max <= total) {
                     model.min = min
+                    var datas = options.rows.slice(min, min + max + 5)
                     for (var i = 0, n = datas.length; i < n; i++) {
                         vm.rows.set(i, datas[i])
                     }
+                    vm.srollTop = top
                 }
-                vm.srollTop = top
             }
         })
         //比要显示的行数多五个
